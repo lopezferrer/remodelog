6ab102546a65e6c28e93440afb02bdd4e9e48345
clopezferrer <lopezferrer@gmail.com>
2022-08-31 00:42:13 -0400

    simple styling

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const works = require('./models/works.js')
 const methodOverride = require('method-override');
 const {render} = require('ejs');
 const mongoose = require('mongoose');
-const Work = require('./models/works.js')
+//const Work = require('./models/works.js')
 const PORT = 3000;
 const mongoURI = 'mongodb://localhost:27017/'+ 'works'
 const db = mongoose.connection
+const worksController = require('./controllers/works.js')
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static('public'));
 mongoose.connect(mongoURI, () => {
   console.log('Connected to Mongod')
 })
 app.use(methodOverride('_method'))
-
+app.use('/works', worksController)
+app.use(express.static('public'));
 
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
 db.on('connected', () => console.log('Mongo connected: ', mongoURI))
@@ -26,65 +28,6 @@ db.on('disconnected', () => console.log('Mongo disconnected'))
 app.get('/', (req, res)=>{
   res.send('Welcome to RemodeLog');
 });
-
-//NEW
-app.get('/works/new', (req, res) => {
-    res.render('new.ejs');
-});
-
-//CREATE
-app.post('/works/', (req, res)=>{
-	Work.create(req.body, (error, createdWork)=>{
-        if (error){
-        	console.log(error);
-        	res.send(error);
-        }
-        else{
-					res.redirect('/works');
-        }
-    });
-});
-
-
-//INDEX
-app.get('/works', (req, res) => {
-	Work.find({}, (error, allWorks) => {
-		res.render('index.ejs', {
-			works: allWorks
-		});
-	});
-});
-
-
-//SHOW
-app.get('/works/:id', (req, res)=>{
-    Work.findById(req.params.id, (err, foundWork)=>{
-        res.render('show.ejs', {
-            work:foundWork
-        });
-    });
-});
-
-//DELETE
-app.delete('/works/:id', (req, res)=>{
-    Work.findByIdAndRemove(req.params.id, (err, data)=>{
-        res.redirect('/works')
-    })
-})
-
-//EDIT
-app.get('/works/:id/edit', (req, res)=>{
-    Work.findById(req.params.id, (err, foundWork)=>{
-        res.render('edit.ejs',{work: foundWork})
-    })
-})
-
-//UPDATE
-app.put('/works/:id', (req, res)=>{
-    Work.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
-		res.redirect(`/works/${req.params.id}`);
-    })
-})
 
 app.listen(PORT, ()=>{
   console.log(`Express listening 🎧 to port ${PORT}`)
---
+++ controllers/works.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const router = express.Router()
+const Work = require('../models/works.js')
+
+//NEW
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+//CREATE
+router.post('/', (req, res)=>{
+	Work.create(req.body, (error, createdWork)=>{
+        if (error){
+        	console.log(error);
+        	res.send(error);
+        }
+        else{
+					res.redirect('/works');
+        }
+    });
+});
+
+//INDEX
+router.get('/', (req, res) => {
+	Work.find({}, (error, allWorks) => {
+		res.render('index.ejs', {
+			works: allWorks
+		});
+	});
+});
+
+//SHOW
+router.get('/:id', (req, res)=>{
+    Work.findById(req.params.id, (err, foundWork)=>{
+        res.render('show.ejs', {
+            work:foundWork
+        });
+    });
+});
+
+//DELETE
+router.delete('/:id', (req, res)=>{
+    Work.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/works')
+    })
+})
+
+//EDIT
+router.get('/:id/edit', (req, res)=>{
+    Work.findById(req.params.id, (err, foundWork)=>{
+        res.render('edit.ejs',{work: foundWork})
+    })
+})
+
+//UPDATE
+router.put('/:id', (req, res)=>{
+    Work.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
+		res.redirect(`/works/${req.params.id}`);
+    })
+})
+
+module.exports = router;
